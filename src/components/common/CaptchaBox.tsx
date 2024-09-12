bb5392d1d5094752bf922f46dc16567ad0c38307
jonixxxxxxxxx <aljonkarlusmartinez48@gmail.com>
2024-09-12 22:02:23 +0800

    feat: add on hover actions

--- src/components/common/CaptchaBox.tsx
+++ src/components/common/CaptchaBox.tsx
@@ -10,16 +10,16 @@ const { box, buttonValue } = useGenerateRandomPosition();
   
   return (
         <div
-          className="border-2 border-white absolute h-[9rem] w-[9rem] grid grid-cols-5 grid-rows-5 gap-0"
+          className="absolute h-[9rem] w-[9rem] grid grid-cols-5 grid-rows-5 gap-0"
           style={{ top: `${box.top}px`, left: `${box.left}px` }}
         >
           { buttonValue?.toContinue && Array.from({ length: 25 }).map((_, index) => (
             <div
               key={index}
-              className="border border-white bg-white bg-opacity-40 w-full h-full flex items-senter justify-center"
+              className="border border-white bg-white bg-opacity-40 w-full h-full flex items-senter justify-center hover:border-sky-700"
             >
                     {/* Triangle shape */}
-                    <div className="border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-b-[20px] border-b-blue-500 hover:bg-sky-700"></div>
+                    <div className="border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-b-[20px] border-b-blue-500"></div>
             </div>
           ))}
         </div>
